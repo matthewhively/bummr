0c5eaca5da20872c48cbdf0f44700b579f0cf09e
Matthew Hively <matthewhively@viz.com>
2025-04-10 17:05:48 -0700

    Suppress commit summary message.

--- lib/bummr/constants.rb
+++ lib/bummr/constants.rb
@@ -2,5 +2,5 @@ module Bummr
  BUNDLE_INSTALL_CMD="bundle install --quiet".freeze
  BUNDLE_EXEC_RAKE="bundle exec rake".freeze
  # TODO: maybe "bundle outdated" ?
- DEFAULT_GIT_COMMIT_CMD="git commit".freeze
+ DEFAULT_GIT_COMMIT_CMD="git commit --quiet".freeze
 end
