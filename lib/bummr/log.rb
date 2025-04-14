3dc80d0a6e0a67f423220bdf0029353cc81013e9
Matthew Hively <matthewhively@viz.com>
2025-04-14 14:43:24 -0700

    Tweak messaging (and msg colors) when gem is not updated to latest

--- lib/bummr/log.rb
+++ lib/bummr/log.rb
@@ -2,7 +2,7 @@ module Bummr
   module Log
     def log(message)
       puts message
-      system("touch log/bummr.log && echo '#{message}' >> log/bummr.log")
+      system("touch log/bummr.log && echo '#{Rainbow.uncolor(message)}' >> log/bummr.log")
     end
   end
 end
